e7e0c42ccf5539b531c43de4d9f8aeacd1143848
anhndt27 <anhndt27@gmail.com>
2020-12-08 21:15:51 +0700

    thu4-9h15p

---
+++ View/Classess/History.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Classess
+{
+    public class History
+    {
+        private string datebuy;
+
+        public History(string datebuy)
+        {
+            DateBuy = datebuy;
+        }
+        public History()
+        {
+
+        }
+        public string DateBuy { get => datebuy; set => datebuy = value; }
+    }
+}
