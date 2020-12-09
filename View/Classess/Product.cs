79d94f8aa2c51be784580cd2d29fb2b4d9da35f4
ndtam1112 <ndtam1010@gmail.com>
2020-12-09 18:34:42 +0700

    layout gio hang va itemcart

--- View/Classess/Product.cs
+++ View/Classess/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace View.Classess
 {
@@ -11,19 +12,22 @@ namespace View.Classess
         private string nameproduct;
         private string typeproduct;
         private string sizeproduct;
-        private byte[] imageproduct;
+        private BitmapImage imageproduct;
+        private string priceproduct;
 
         public string NameProduct { get => nameproduct; set => nameproduct = value; }
         public string TypeProduct { get => typeproduct; set => typeproduct = value; }
         public string SizeProduct { get => sizeproduct; set => sizeproduct = value; }
-        public string ImageProduct { get => sizeproduct; set => sizeproduct = value; }
+        public BitmapImage ImageProduct { get => imageproduct; set => imageproduct = value; }
+        public string PriceProduct { get => priceproduct; set => priceproduct = value; }
 
-        public Product(string nameproduct, string typeproduct, string sizeproduct, string imageproduct)
+        public Product(string nameproduct, string typeproduct, string sizeproduct, BitmapImage imageproduct)
         {
             NameProduct = nameproduct;
             TypeProduct = typeproduct;
             SizeProduct = sizeproduct;
             ImageProduct = imageproduct;
+            PriceProduct = priceproduct;
 
         }
         public Product()
